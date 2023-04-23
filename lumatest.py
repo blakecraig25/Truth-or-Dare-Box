35d6de8baa05755ce8afe49af78c73beef18f1da
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-22 21:34:42 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -9,7 +9,7 @@ serial = spi(port=0, device=0, gpio_DC=22, gpio_RST=27)
 device = st7735(serial, rotate=0)
 
 # create an image and draw a rectangle and text on it
-image = Image.new(mode="RGB", size=device.size, color=(0, 0, 0))
+image = Image.new(mode="RGB", size=device.size, color=(255, 255, 255))
 draw = ImageDraw.Draw(image)
 draw.rectangle((10, 10, device.width - 10, device.height - 10), outline="white", fill="black")
 draw.text((20, 30), "Hello, world!", font=None, fill="white")
