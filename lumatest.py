fb563170a5a3bdebe86923fc15be835cc8faa0c7
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-22 21:52:43 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -1,5 +1,6 @@
 from luma.core.interface.serial import spi
 from luma.lcd.device import st7735
+from PIL import Image, ImageDraw, ImageFont
 
 # create SPI interface
 serial = spi(port=0, device=0, gpio_DC=22, gpio_RST=27)
@@ -7,14 +8,12 @@ serial = spi(port=0, device=0, gpio_DC=22, gpio_RST=27)
 # create LCD device
 device = st7735(serial, rotate=0)
 
-# set background color
-device.clear()
+# create an image and draw text on it
+image = Image.new(mode="RGB", size=device.size, color="white")
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("arial.ttf", size=16)
+draw.text((10, 10), "Hello, world!", font=font, fill="black")
 
-# set text color
-device.text_color = (255, 255, 255)
+# display the image on the LCD
+device.display(image)
 
-# display text
-device.draw_text(10, 10, "Hello, World!")
-
-# update display
-device.display()
