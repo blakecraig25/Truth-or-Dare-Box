058c99b11bb55614c89d172077ba737a6492387d
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-23 20:34:45 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -18,16 +18,18 @@ DC = 25
 SPI_PORT = 0
 SPI_DEVICE = 0
 
+# Create a new SPI device object.
+spi = SPI.SpiDev(SPI_PORT, SPI_DEVICE)
+
 # Create TFT LCD display class.
 disp = ST7735.ST7735(
-    port=SPI_PORT,
-    device=SPI_DEVICE,
+    port=0,
     cs=GPIO.OUT,
     dc=GPIO.OUT,
     rst=GPIO.OUT,
     width=128,
     height=160,
-    rotation=0)
+    spi=spi)
 
 # Initialize display.
 disp.begin()
