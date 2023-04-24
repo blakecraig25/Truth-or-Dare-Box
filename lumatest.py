b7c8922064dd8a1e0820b78adbf01998ba8ae32d
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-23 22:30:00 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -1,86 +1,37 @@
-#!/usr/bin/env python
+import board
+import digitalio
+import adafruit_rgb_display.st7789 as st7789
+from PIL import Image, ImageDraw, ImageFont
 
-import time
-import spidev
-import RPi.GPIO as GPIO
-from PIL import Image
-GPIO.setwarnings(False)
-
-# ST7789V LCD display parameters
-WIDTH = 240
-HEIGHT = 320
-SPEED_HZ = 32000000
-
-# Raspberry Pi pin configuration:
-RST = 27
-DC = 25
+# Define the SPI and pins
 SPI_PORT = 0
 SPI_DEVICE = 0
+CS_PIN = digitalio.DigitalInOut(board.CE0)
+DC_PIN = digitalio.DigitalInOut(board.D25)
+RESET_PIN = digitalio.DigitalInOut(board.D24)
 
-# Initialize SPI bus and display
-spi = spidev.SpiDev()
-spi.open(SPI_PORT, SPI_DEVICE)
-spi.max_speed_hz = SPEED_HZ
+# Initialize the display
+display = st7789.ST7789(
+    board.SPI(),
+    cs=CS_PIN,
+    dc=DC_PIN,
+    rst=RESET_PIN,
+    baudrate=24000000,
+    width=240,
+    height=240,
+    x_offset=0,
+    y_offset=80,
+)
 
-# Initialize GPIO pins
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(RST, GPIO.OUT)
-GPIO.setup(DC, GPIO.OUT)
+# Initialize the buffer
+image = Image.new("RGB", (240, 240), (0, 0, 0))
+draw = ImageDraw.Draw(image)
 
-# Reset display
-GPIO.output(RST, GPIO.HIGH)
-time.sleep(0.1)
-GPIO.output(RST, GPIO.LOW)
-time.sleep(0.1)
-GPIO.output(RST, GPIO.HIGH)
-time.sleep(0.1)
+# Set the font
+font = ImageFont.load_default()
 
-# Send initialization commands to display
-GPIO.output(DC, GPIO.LOW)
-spi.writebytes([0x36, 0x00]) # Memory Data Access Control
-GPIO.output(DC, GPIO.HIGH)
-spi.writebytes([0x3A, 0x05]) # Interface Pixel Format
-GPIO.output(DC, GPIO.LOW)
-spi.writebytes([0xB2, 0x0C, 0x0C, 0x00, 0x33, 0x33]) # Porch Setting
-spi.writebytes([0xB7, 0x35]) # Gate Control
-spi.writebytes([0xBB, 0x2B]) # VCOM Setting
-spi.writebytes([0xC0, 0x2C]) # LCM Control
-spi.writebytes([0xC2, 0x01, 0xFF]) # VDV and VRH Command Enable
-spi.writebytes([0xC3, 0x11]) # VRH Set
-spi.writebytes([0xC4, 0x20]) # VDV Set
-spi.writebytes([0xC6, 0x0F]) # Frame Rate Control
-spi.writebytes([0xD0, 0xA4, 0xA1]) # Power Control 1
-spi.writebytes([0xE0, 0xD0, 0x04, 0x0D, 0x11, 0x13, 0x2B, 0x3F, 0x54, 0x4C, 0x18, 0x0D, 0x0B, 0x1F, 0x23]) # Positive Gamma Control
-spi.writebytes([0xE1, 0xD0, 0x04, 0x0C, 0x11, 0x13, 0x2C, 0x3F, 0x44, 0x51, 0x2F, 0x1F, 0x1F, 0x20, 0x23]) # Negative Gamma Control
-spi.writebytes([0x21]) # Display Inversion On
-spi.writebytes([0x11]) # Sleep Out
-time.sleep(0.1)
-spi.writebytes([0x29]) # Display On
+# Draw some text
+draw.text((20, 20), "Hello, world!", font=font, fill=(255, 255, 255))
 
-# Clear display
-GPIO.output(DC, GPIO.LOW)
-spi.writebytes([0x2C])
-for i in range(WIDTH * HEIGHT):
-    spi.writebytes([0x00, 0x00])
-
-def set_window(x0, y0, x1, y1):
-    GPIO.output(DC, GPIO.LOW)
-    spi.writebytes([0x2A])
-    GPIO.output(DC, GPIO.HIGH)
-    spi.writebytes([x0 >> 8, x0 & 0xFF, x1 >> 8, x1 & 0xFF])
-    GPIO.output(DC, GPIO.LOW)
-    spi.writebytes([0x2B])
-    GPIO.output(DC, GPIO.HIGH)
-    spi.writebytes([y0 >> 8, y0 & 0xFF, y1 >> 8, y1 & 0xFF])
-    GPIO.output(DC, GPIO.LOW)
-    spi.writebytes([0x2C])
-    GPIO.output(DC, GPIO.HIGH)
-
-def draw_pixel(x, y, color):
-    set_window(x, y, x, y)
-    GPIO.output(DC, GPIO.HIGH)
-    spi.writebytes([(color >> 8) & 0xFF, color & 0xFF])
-
-# Example usage:
-draw_pixel(0, 0, 0xFF00)  # Draws a green pixel at the top-left corner of the screen
+# Display the image on the screen
+display.image(image)
