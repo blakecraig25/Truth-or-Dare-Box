d3b0d4823e0cf83a9dc032f0450bb2bbb66baab1
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-23 21:13:01 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -1,50 +1,61 @@
 #!/usr/bin/env python
 
 import time
+import spidev
+import RPi.GPIO as GPIO
 
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+# ST7789V LCD display parameters
+WIDTH = 240
+HEIGHT = 320
+SPEED_HZ = 32000000
 
 # Raspberry Pi pin configuration:
-RST = None     # Not used for I2C interface
-DC = 23
+RST = 27
+DC = 25
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-# OLED display height and width in pixels
-DISP_WIDTH = 128
-DISP_HEIGHT = 32
+# Initialize SPI bus and display
+spi = spidev.SpiDev()
+spi.open(SPI_PORT, SPI_DEVICE)
+spi.max_speed_hz = SPEED_HZ
 
-# Create OLED display class
-disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+# Initialize GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(RST, GPIO.OUT)
+GPIO.setup(DC, GPIO.OUT)
 
-# Initialize OLED display
-disp.begin()
+# Reset display
+GPIO.output(RST, GPIO.HIGH)
+time.sleep(0.1)
+GPIO.output(RST, GPIO.LOW)
+time.sleep(0.1)
+GPIO.output(RST, GPIO.HIGH)
+time.sleep(0.1)
+
+# Send initialization commands to display
+GPIO.output(DC, GPIO.LOW)
+spi.writebytes([0x36, 0x00]) # Memory Data Access Control
+GPIO.output(DC, GPIO.HIGH)
+spi.writebytes([0x3A, 0x05]) # Interface Pixel Format
+GPIO.output(DC, GPIO.LOW)
+spi.writebytes([0xB2, 0x0C, 0x0C, 0x00, 0x33, 0x33]) # Porch Setting
+spi.writebytes([0xB7, 0x35]) # Gate Control
+spi.writebytes([0xBB, 0x2B]) # VCOM Setting
+spi.writebytes([0xC0, 0x2C]) # LCM Control
+spi.writebytes([0xC2, 0x01, 0xFF]) # VDV and VRH Command Enable
+spi.writebytes([0xC3, 0x11]) # VRH Set
+spi.writebytes([0xC4, 0x20]) # VDV Set
+spi.writebytes([0xC6, 0x0F]) # Frame Rate Control
+spi.writebytes([0xD0, 0xA4, 0xA1]) # Power Control 1
+spi.writebytes([0xE0, 0xD0, 0x04, 0x0D, 0x11, 0x13, 0x2B, 0x3F, 0x54, 0x4C, 0x18, 0x0D, 0x0B, 0x1F, 0x23]) # Positive Gamma Control
+spi.writebytes([0xE1, 0xD0, 0x04, 0x0C, 0x11, 0x13, 0x2C, 0x3F, 0x44, 0x51, 0x2F, 0x1F, 0x1F, 0x20, 0x23]) # Negative Gamma Control
+spi.writebytes([0x21]) # Display Inversion On
+spi.writebytes([0x11]) # Sleep Out
+time.sleep(0.1)
+spi.writebytes([0x29]) # Display On
 
 # Clear display
-disp.clear()
-disp.display()
-
-# Create blank image for drawing
-image = Image.new('1', (DISP_WIDTH, DISP_HEIGHT))
-
-# Get drawing object to draw on image
-draw = ImageDraw.Draw(image)
-
-# Draw some text
-font = ImageFont.load_default()
-draw.text((0,0), "Hello World!", font=font, fill=255)
-
-# Display image on OLED
-disp.image(image)
-disp.display()
-
-time.sleep(5.0)
-
-# Clear display
-disp.clear()
-disp.display()
+GPIO.output(DC, GPIO.LOW)
+spi.writebytes
