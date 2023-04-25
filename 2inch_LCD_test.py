90e3d15bd553dadc2161d5584834c1f81076a052
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-25 16:47:40 -0400

    Add files via upload

--- 2inch_LCD_test.py
+++ 2inch_LCD_test.py
@@ -17,66 +17,73 @@ BL = 18
 bus = 0
 device = 0
 logging.basicConfig(level=logging.DEBUG)
-try:
-    # display with hardware SPI:
-    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
-    #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-    disp = LCD_2inch.LCD_2inch()
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
+for i in range(3):
+    if i == 0:
+        text1 = 'Hello'
+    if i == 1:
+        text1 = 'There'
+    if i == 2:
+        text1 = 'General Kenobi'    
+    try:
+        # display with hardware SPI:
+        ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+        #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+        disp = LCD_2inch.LCD_2inch()
+        # Initialize library.
+        disp.Init()
+        # Clear display.
+        disp.clear()
 
-    # Create blank image for drawing.
-    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
-    draw = ImageDraw.Draw(image1)
+        # Create blank image for drawing.
+        image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+        draw = ImageDraw.Draw(image1)
 
-    logging.info("draw point")
+        # logging.info("draw point")
 
-    draw.rectangle((5,10,6,11), fill = "BLACK")
-    draw.rectangle((5,25,7,27), fill = "BLACK")
-    draw.rectangle((5,40,8,43), fill = "BLACK")
-    draw.rectangle((5,55,9,59), fill = "BLACK")
+        # draw.rectangle((5,10,6,11), fill = "BLACK")
+        # draw.rectangle((5,25,7,27), fill = "BLACK")
+        # draw.rectangle((5,40,8,43), fill = "BLACK")
+        # draw.rectangle((5,55,9,59), fill = "BLACK")
 
-    logging.info("draw line")
-    draw.line([(20, 10),(70, 60)], fill = "RED",width = 1)
-    draw.line([(70, 10),(20, 60)], fill = "RED",width = 1)
-    draw.line([(170,15),(170,55)], fill = "RED",width = 1)
-    draw.line([(150,35),(190,35)], fill = "RED",width = 1)
+        # logging.info("draw line")
+        # draw.line([(20, 10),(70, 60)], fill = "RED",width = 1)
+        # draw.line([(70, 10),(20, 60)], fill = "RED",width = 1)
+        # draw.line([(170,15),(170,55)], fill = "RED",width = 1)
+        # draw.line([(150,35),(190,35)], fill = "RED",width = 1)
 
-    logging.info("draw rectangle")
-    draw.rectangle([(20,10),(70,60)],fill = "WHITE",outline="BLUE")
-    draw.rectangle([(85,10),(130,60)],fill = "BLUE")
+        # logging.info("draw rectangle")
+        # draw.rectangle([(20,10),(70,60)],fill = "WHITE",outline="BLUE")
+        # draw.rectangle([(85,10),(130,60)],fill = "BLUE")
 
-    logging.info("draw circle")
-    draw.arc((150,15,190,55),0, 360, fill =(0,255,0))
-    draw.ellipse((150,65,190,105), fill = (0,255,0))
+        # logging.info("draw circle")
+        # draw.arc((150,15,190,55),0, 360, fill =(0,255,0))
+        # draw.ellipse((150,65,190,105), fill = (0,255,0))
 
-    logging.info("draw text")
-    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
-    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
-    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+        logging.info("draw text")
+        Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
+        Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
+        Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
 
-    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
-    draw.text((5, 68), 'Give me a truth or dare\nI want one now', fill = "BLACK",font=Font3)
-    draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
-    text= u"Hello"
-    draw.text((5, 200),text, fill = "BLUE",font=Font3)
-    image1=image1.rotate(180)
-    disp.ShowImage(image1)
-    time.sleep(3)
+        draw.rectangle([(0,65),(140,100)],fill = "WHITE")
+        draw.text((5, 68), 'Give me a truth or dare\nI want one now', fill = "BLACK",font=Font3)
+        draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
+        text= u"Hello"
+        draw.text((5, 200),text1, fill = "BLUE",font=Font3)
+        image1=image1.rotate(180)
+        disp.ShowImage(image1)
+        time.sleep(3)
 
-    # logging.info("show image")
-    # image = Image.open('../pic/LCD_2inch.jpg')
-    # image = image.rotate(180)
-    # disp.ShowImage(image)
-    # time.sleep(3)
+        # logging.info("show image")
+        # image = Image.open('../pic/LCD_2inch.jpg')
+        # image = image.rotate(180)
+        # disp.ShowImage(image)
+        # time.sleep(3)
 
-    disp.module_exit()
-    logging.info("quit:")
-except IOError as e:
-    logging.info(e)
-except KeyboardInterrupt:
-    disp.module_exit()
-    logging.info("quit:")
-    exit()
+        disp.module_exit()
+        logging.info("quit:")
+    except IOError as e:
+        logging.info(e)
+    except KeyboardInterrupt:
+        disp.module_exit()
+        logging.info("quit:")
+        exit()
