04584d00bac0c85bf28e4eecaae2700ec7cf6641
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-25 19:21:27 -0400

    Add files via upload

--- ToD_LCDOnly.py
+++ ToD_LCDOnly.py
@@ -22,36 +22,6 @@ bus = 0
 device = 0
 logging.basicConfig(level=logging.DEBUG)
 
-def image(text):
-    #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
-    disp = LCD_2inch.LCD_2inch()
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
-
-    # Create blank image for drawing.
-    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
-    draw = ImageDraw.Draw(image1)
-    
-
-    logging.info("draw text")
-    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
-    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
-    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
-
-    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
-    draw.text((5, 68), text, fill = "BLACK",font=Font3)
-    draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
-    text= u"Hello"
-    draw.text((5, 200),text, fill = "BLUE",font=Font3)
-    image1=image1.rotate(180)
-    disp.ShowImage(image1)
-    time.sleep(3)
-
-    disp.module_exit()
-    logging.info("quit:")
-
 def get_question(ToD, rating):
     if ToD != "T" and ToD != "D":
         print("Error: Invalid Input for Truth Or Dare.")
@@ -73,12 +43,17 @@ def get_question(ToD, rating):
 
 while True:
     text_start = "Starting truth or dare game..."
-    image(text_start)
-    time.sleep(3)
+    
+    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+    draw = ImageDraw.Draw(image1)
+    logging.info("draw text")
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
+    draw.text((5, 68), text_start, fill = "BLACK",font=Font3)
+
     text_q1 = "Please enter the desired game mode:\n't' for Truth\n'd' for Dare"
     # wait for T or D key to be pressed to select Truth or Dare
-    image(text_q1)
-
+    
     while True:
         if keyboard.is_pressed('t'):
             ToD = "T"
@@ -89,8 +64,7 @@ while True:
     
     text_q2 = "Please enter the desired rating:\n'e' for PG\n'm' for PG13\n'h' for R"
     # wait for PG, PG13, or R key to be pressed to select the rating
-    image(text_q2)
-
+    
     while True:
         if keyboard.is_pressed('e'):
             rating = "PG"
@@ -105,7 +79,7 @@ while True:
     question = get_question(ToD, rating)
     if question:
         text_response = question + "\n\nWould you like to keep playing?\n Press 'y' to continue or 'n' to stop."
-        image(text_response)
+        print(text_response)
 
     # wait for Y or N key to be pressed to continue
     while True:
