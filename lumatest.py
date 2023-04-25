6f6adaa9e0d24a10b4519a1f05c6c5e52358ae4c
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-25 19:36:19 -0400

    Add files via upload

--- lumatest.py
+++ lumatest.py
@@ -1,21 +1,119 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#import chardet
+import os
+import sys
 import time
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-from st7789v import ST7789V
+import logging
+import spidev as SPI
+sys.path.append("..")
+from lib import LCD_2inch
+from PIL import Image,ImageDraw,ImageFont
 
-# create an instance of the ST7789V display with the correct settings for your display
-display = ST7789V(port=0, cs=0, dc=9, backlight=None, rotation=0, spi_speed_hz=80 * 1000 * 1000)
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0
+device = 0
+logging.basicConfig(level=logging.DEBUG)
+try:
+    # display with hardware SPI:
+    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+    #disp = LCD_2inch.LCD_2inch(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp = LCD_2inch.LCD_2inch()
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
 
-# create a new PIL image and a drawing object
-image = Image.new("RGB", (display.width, display.height), "white")
-draw = ImageDraw.Draw(image)
+    # Create blank image for drawing.
+    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+    draw = ImageDraw.Draw(image1)
 
-# choose a font (you can download a font file and place it in your project directory)
-font = ImageFont.load_default()
+    # logging.info("draw point")
 
-# draw some text on the image
-draw.text((10, 10), "Hello, world!", font=font, fill=(0, 0, 0))
+    # draw.rectangle((5,10,6,11), fill = "BLACK")
+    # draw.rectangle((5,25,7,27), fill = "BLACK")
+    # draw.rectangle((5,40,8,43), fill = "BLACK")
+    # draw.rectangle((5,55,9,59), fill = "BLACK")
 
-# display the image on the screen
-display.display(image)
+    # logging.info("draw line")
+    # draw.line([(20, 10),(70, 60)], fill = "RED",width = 1)
+    # draw.line([(70, 10),(20, 60)], fill = "RED",width = 1)
+    # draw.line([(170,15),(170,55)], fill = "RED",width = 1)
+    # draw.line([(150,35),(190,35)], fill = "RED",width = 1)
+
+    # logging.info("draw rectangle")
+    # draw.rectangle([(20,10),(70,60)],fill = "WHITE",outline="BLUE")
+    # draw.rectangle([(85,10),(130,60)],fill = "BLUE")
+
+    # logging.info("draw circle")
+    # draw.arc((150,15,190,55),0, 360, fill =(0,255,0))
+    # draw.ellipse((150,65,190,105), fill = (0,255,0))
+
+    logging.info("draw text")
+    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
+    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+
+    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
+    draw.text((5, 68), 'Give me a truth or dare\nI want one now', fill = "BLACK",font=Font3)
+    draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
+    text= u"Hello"
+    draw.text((5, 200),text, fill = "BLUE",font=Font3)
+    image1=image1.rotate(180)
+    disp.ShowImage(image1)
+    time.sleep(3)
+
+    # logging.info("show image")
+    # image = Image.open('../pic/LCD_2inch.jpg')
+    # image = image.rotate(180)
+    # disp.ShowImage(image)
+    # time.sleep(3)
+
+    
+
+    disp.module_exit()
+    logging.info("quit:")
+except IOError as e:
+    logging.info(e)
+except KeyboardInterrupt:
+    disp.module_exit()
+    logging.info("quit:")
+    exit()
+
+try:
+    disp = LCD_2inch.LCD_2inch()
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
+
+    # Create blank image for drawing.
+    image1 = Image.new("RGB", (disp.height, disp.width ), "WHITE")
+    draw = ImageDraw.Draw(image1)
+
+    logging.info("draw text")
+    Font1 = ImageFont.truetype("../Font/Font01.ttf",25)
+    Font2 = ImageFont.truetype("../Font/Font01.ttf",35)
+    Font3 = ImageFont.truetype("../Font/Font02.ttf",32)
+
+    draw.rectangle([(0,65),(140,100)],fill = "WHITE")
+    draw.text((5, 68), 'Give me a truth or dare\nI want one now', fill = "BLACK",font=Font3)
+    draw.text((5, 160), '1234567890', fill = "GREEN",font=Font3)
+    text= u"Hello"
+    draw.text((5, 200), 'Did this work?', fill = "BLUE",font=Font3)
+    image1=image1.rotate(180)
+    disp.ShowImage(image1)
+    time.sleep(3)
+
+    disp.module_exit()
+    logging.info("quit:")
+except IOError as e:
+    logging.info(e)
+except KeyboardInterrupt:
+    disp.module_exit()
+    logging.info("quit:")
+    exit()
+
