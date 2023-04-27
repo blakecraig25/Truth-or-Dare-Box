9bff5207dcc3ec7915cb7ff5c6ecdcf3c705d76a
blakecraig25 <112400887+blakecraig25@users.noreply.github.com>
2023-04-27 13:36:06 -0400

    Add files via upload

--- st7789_test.py
+++ st7789_test.py
@@ -1,18 +1,14 @@
-"""Display all possible rotation/mirroring combinations."""
-import time
-from st7789v.interface import RaspberryPi
-from st7789v import Display
+i = 1
+image = 0
+def money(j, image1):
+    if j < 2:
+        image1 = 1000
+    if j == 2:
+        image1 += 1
+    return image1
 
-black, white = [(0, 0, 0)], [(255, 255, 255)]
-red, green, blue = [(255, 0, 0)], [(0, 255, 0)], [(0, 0, 255)]
-line = red * 64 + white * 24 + green * 64 + white * 24 + blue * 64
 
-with RaspberryPi() as rpi:
-    display = Display(rpi)
-    for mirrored in (False, True):
-        for rotation in (0, 90, 180, 270):
-            display.initialize(rotation=rotation, mirrored=mirrored)
-            display.draw_rgb_bytes(black * 240 * 320)
-            row = line + black * 80 if display.max_w == 320 else line
-            display.draw_rgb_bytes(row * 64)
-            time.sleep(1)
+while i < 3:
+    image = money(i, image)
+    print(image)
+    i += 1
